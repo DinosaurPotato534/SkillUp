ecf8cdb139bb39e51b344d2b24383506fda23a99
lord <yrnlehjend@gmail.com>
2023-12-17 15:28:52 +0000

    integrate

--- frontend/src/Pages/MentorsPage.js
+++ frontend/src/Pages/MentorsPage.js
@@ -1,31 +1,4 @@
-// import "../Styles/Mentors.css";
-// import MentorsComponent from "../Components/Mentors/FindMentorsComponent";
-// import AcceptMentors from "../Components/Mentors/AcceptMentors";
-// import * as React from 'react';
-// import AcceptStudents from "../Components/Mentors/AcceptStudents";
-// import Students from "../Components/Mentors/Students";
-// import MeetingPrompt from "../Components/Mentors/MeetingPrompt";
-// import Header from "../Components/Mentors/Header";
-
-
-// function Mentors() {
-//   return (
-//     <div className="container">
-//       <MentorsComponent /> 
-//       <AcceptMentors/> 
-//       <AcceptStudents/>
-//       <Students />
-//       <Header />
-//       <MeetingPrompt />
-//     </div>
-//   );
-// }
-
-// export default Mentors;
-
-
 import { Routes, Route, Outlet } from "react-router-dom";
-// import MentorsComponent from "../Components/Mentors/FindMentorsComponent";
 import AcceptMentors from "../Components/Mentors/AcceptMentors";
 import AcceptStudents from "../Components/Mentors/AcceptStudents";
 import Students from "../Components/Mentors/Students";
