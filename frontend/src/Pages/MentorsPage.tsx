b6b62c1dda56c57942687c00499164a7f62e1718
lord <yrnlehjend@gmail.com>
2023-12-17 12:11:41 +0000

    add page for mentee request

--- frontend/src/Pages/MentorsPage.tsx
+++ frontend/src/Pages/MentorsPage.tsx
@@ -2,12 +2,16 @@ import "../Styles/Mentors.css";
 import MentorsComponent from "../Components/Mentors/FindMentorsComponent";
 import AcceptMentors from "../Components/Mentors/AcceptMentors";
 import * as React from 'react';
+import AcceptStudents from "../Components/Mentors/AcceptStudents";
+import Students from "../Components/Mentors/Students";
 
 function Mentors() {
   return (
     <div className="container">
       {/* <MentorsComponent /> */}
-      <AcceptMentors/>
+      {/* <AcceptMentors/> */}
+      {/* <AcceptStudents/> */}
+      <Students />
     </div>
   );
 }
